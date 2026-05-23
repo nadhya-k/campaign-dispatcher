6004a74164f3516ee5a9736256d8765b00afac43
nadhya-k <nadhya.k@gmail.com>
2026-05-23 21:40:12 +0100

    updated jest.config.ts to add transformIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  transformIgnorePatterns: ["node_modules/(?!(uuid)/)"],
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.test.ts"],
   collectCoverageFrom: ["src/**/*.ts", "!src/__tests__/**", "!src/index.ts"],
